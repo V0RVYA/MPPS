181cdd95eaf353545258f5fced44316f3d6139a0
V0rvya <vorobevamaria@proton.me>
2025-02-14 18:12:05 -0500

    novice model done

--- piecemeal.py
+++
@@ -1,6 +0,0 @@
-            if faculty == A and president == R: AR+=1
-            if faculty == A and president == B: AB+=1
-            if faculty == H and president == R: HR+=1
-            if faculty == H and president == B: HB+=1
-            if faculty == S and president == R: SR+=1
-            if faculty == S and president == B: SB+=1
